11b556b98ed0ca5f9423bc0c8324828f3f76ab83
Tom <tsechrist@outlook.com>
2019-05-27 23:32:14 -0500

    firebase initialization code imported from console

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCY7FgHIfLvs0qC3oayCjCGpo6FoJH_pW8",
+  authDomain: "hw-time-table.firebaseapp.com",
+  databaseURL: "https://hw-time-table.firebaseio.com",
+  projectId: "hw-time-table",
+  storageBucket: "hw-time-table.appspot.com",
+  messagingSenderId: "861438962218",
+  appId: "1:861438962218:web:16a2b53d637799f3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
