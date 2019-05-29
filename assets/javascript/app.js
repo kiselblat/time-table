96d03c7534676a36e499a7b3f081c46cb2d04d4c
Tom <tsechrist@outlook.com>
2019-05-28 23:51:02 -0500

    Relevent train information now displays properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,13 +10,14 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-console.log("firebase intialized")
+
 var database = firebase.database();
 
+// var firstTime = moment($("#firstTime").val().trim(), "hh:mm").subtract(1, "years").format("X");
 
 var addTrain = function() {
   
-  var trainName = $("#input-name").val().trim();
+  var trainName = $('#input-name').val().trim();
   var trainDestination = $('#input-destination').val().trim();
   var trainFirstTrip = $('#input-first-trip').val().trim();
   var trainFrequency = $('#input-frequency').val().trim();
@@ -51,4 +52,40 @@ $(document).ready(function() {
     console.log("Click!");
     addTrain();
   });
+  
+  database.ref().on('child_added' , function(childSnapshot) {
+    console.log(childSnapshot.val());
+    
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().destination;
+    var trainFirstTrip = childSnapshot.val().firstTrip
+    var trainFrequency = childSnapshot.val().frequency;
+
+    var trainTime = moment(trainFirstTrip , 'HH:mm').subtract(1, 'years').format('HH:mm');
+
+    var deltaT = moment().diff(moment(trainTime , 'HH:mm') , 'minutes');
+    var remaining = deltaT % trainFrequency;
+    var timeUntil = trainFrequency - remaining;
+    var nextTrain = moment().add(timeUntil , 'minutes').format('HH:mm');
+
+    console.log("---------Train Data---------")
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(trainTime);
+    console.log(trainFrequency);
+    console.log(nextTrain);
+    console.log(timeUntil);
+
+    var lineName = $('<td/>').text(trainName);
+    var serviceTo = $('<td/>').text(trainDestination);
+    var runningEvery = $('<td/>').text(trainFrequency + "min");
+    var nextArrival = $('<td/>').text(nextTrain + "hrs");
+    var eta = $('<td/>').text(timeUntil + "mins");
+
+    var newRow = $('<tr/>').append(lineName , serviceTo , runningEvery , nextArrival , eta);
+    
+    $('#schedule-table > tbody').append(newRow);
+
+  })
+
 });
